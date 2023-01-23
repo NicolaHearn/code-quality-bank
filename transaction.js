a68358c447c02b6d4df28b7712e60d23bc8df998
nfhearn@gmail.com <nicolahearn@mastude5h3vq6l4.home>
2023-01-23 15:50:50 +0000

    passes unit test - transaction object has 4 properties set to null when initialised

--- transaction.js
+++ transaction.js
@@ -1,6 +1,9 @@
 class Transaction {
   constructor() {
-
+    this.date = null;
+    this.credit_amount = null;
+    this.debit_amount = null;
+    this.balance = null;
   }
 }
 
--- transaction.test.js
+++ transaction.test.js
@@ -1,9 +1,22 @@
 const Transaction = require('./transaction.js');
 
 describe(Transaction, () => {
-  it('initializes a new transaction object', () => {
-    const transaction = new Transaction();
+  describe('initialises', () => {
+    it('initializes a new transaction object', () => {
+      const transaction = new Transaction();
 
-    expect(transaction instanceof Transaction).toBe(true);
-  })
+      expect(transaction instanceof Transaction).toBe(true);
+    });
+
+    it('has 4 properties all set to null: date, credit_amount, debit_amount, balance', () => {
+      const transaction = new Transaction();
+
+      expect(transaction.date).toEqual(null);
+      expect(transaction.credit_amount).toEqual(null);
+      expect(transaction.debit_amount).toEqual(null);
+      expect(transaction.balance).toEqual(null);
+    });
+  });
+
+  
 })
