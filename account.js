ac2b5c06730f1aaac522863eb9c3e831f2fecfab
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2023-01-23 15:06:42 +0000

    passes test - Account.print_statement() returns a header row

--- account.js
+++ account.js
@@ -3,6 +3,12 @@ class Account {
     this.current_balance = 0
     this.transactions = [];
   }
+
+  printStatement() {
+
+    const header = "date || credit || debit || balance";
+    return header;
+  }
 }
 
 module.exports = Account;
--- account.test.js
+++ account.test.js
@@ -2,12 +2,25 @@ const Transaction = require('./transaction.js');
 const Account = require('./account.js')
 
 describe('account', () => {
-  it('initializes a new object (account) with a starting balance of zero and an empty array for storing transactions', () => {
-    const myAccount = new Account(); 
-    expect(myAccount instanceof Account).toBe(true);
-    expect(myAccount.current_balance).toEqual(0);
-    expect(myAccount.transactions instanceof Array).toBe(true);
-  })
+  describe('initializes', () => {
+    it('initializes a new object (account) with a starting balance of zero and an empty array for storing transactions', () => {
+      const myAccount = new Account(); 
+      expect(myAccount instanceof Account).toBe(true);
+      expect(myAccount.current_balance).toEqual(0);
+      expect(myAccount.transactions instanceof Array).toBe(true);
+    });
+  });
+
+  describe('print_statement', () => {
+    it('returns a header for the statement', () => {
+      const MyAccount = new Account();
+      expect(MyAccount.printStatement()).toEqual(expect.stringContaining("date || credit || debit || balance"));
+    })
+  });
 
 
-})
+
+});
+
+
+
